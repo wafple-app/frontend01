fd4e9b79afec2fec32c170062e1d34fc19af4403
y-jaykim <yeonjae.jay.kim@gmail.com>
2022-03-12 16:02:33 -0800

    global style modified and recent history moved

--- src/styles/common.js
+++ src/styles/common.js
@@ -1,2 +1,42 @@
 import { StyleSheet } from "react-native";
-const styles = StyleSheet.create({})
+
+const globalStyles = {
+    //common style
+    inr: {
+        height: 46,
+        marginHorizontal: 16,
+        flexDirection: 'row',
+        justifyContent: 'flex-start',
+        alignItems: 'center',
+        position: 'relative'
+    },
+    positionRight: {
+    position: 'absolute',
+    right: 0,
+    top: 15,
+    },
+    positionLeft: {
+        position: 'absolute',
+        left: 0,
+        top: 10,
+    },
+    font16_R : {
+        fontSize: 16, 
+        color: '#222'
+    },
+    font14_R : {
+        fontSize: 14, 
+        color: '#444',
+        lineHeight: 24
+    },
+    font18_B : {
+        fontSize: 18, 
+        fontWeight: '700',
+        color: '#222'
+    },
+    spaceBetween : {
+    justifyContent: 'space-between'
+    },
+};
+
+export default globalStyles;
